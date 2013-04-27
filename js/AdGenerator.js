fcf57a3c0eba0d62f8dbeaa388993bf063d7e36a
Christopher Kawasaki <cmkawasaki@gmail.com>
2013-04-27 14:48:13 -0700

    Adding Ad Generator

---
+++ js/AdGenerator.js
@@ -0,0 +1,9 @@
+function AdGenerator() {
+
+}
+
+AdGenerator.prototype = Object.create(null);
+AdGenerator.prototype.constructor = AdGenerator;
+
+var _ads = ["Feeling out of Shape?  Try our new Circle Peg!",
+    "Are you a bit Round?  Try our new Square program!  Guaranteed to tone your body in 30 days or your money back!"];
