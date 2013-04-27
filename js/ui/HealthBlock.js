b9d3c54e28a2b75b21f3df3c39b100839148a6ca
Tyler <tarehart@gmail.com>
2013-04-27 16:56:10 -0700

    Health block takes an element id

--- js/ui/HealthBlock.js
+++ js/ui/HealthBlock.js
@@ -7,10 +7,12 @@ function clamp(val, max) {
     return val;
 }
 
-function HealthBlock() {
+function HealthBlock(blockElementId) {
     this.red = 0;
     this.green = 0;
     this.blue = 0;
+
+    this.element = document.getElementById(blockElementId);
     
     return this;
 }
@@ -19,13 +21,16 @@ HealthBlock.prototype = Object.create(null);
 HealthBlock.prototype.constructor = HealthBlock;
 
 Object.defineProperties(HealthBlock.prototype, {
-    applyColor: {
+    addColor: {
         // We're expecting values ranging from 0 to 255.
-        value: function applyColor(r, g, b) {
+        value: function addColor(r, g, b) {
             this.red = clamp(this.red + r, 255);
             this.green = clamp(this.green + g, 255);
             this.blue = clamp(this.blue + b, 255);
-            return this.getColor();
+
+            this.element.style.backgroundColor = "rgb(" + this.red + ", " + this.green + ", " + this.blue + ")"
+
+            return this;
         }
     },
     isWhite: {
