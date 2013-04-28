2fc8b14a7cec78f6bf3c857ef678d866b5ab63da
Matt Rakow <matt.rakow@gmail.com>
2013-04-27 19:26:04 -0700

    fixing bad merge

--- js/TouchPointManager.js
+++ js/TouchPointManager.js
@@ -1,7 +1,5 @@
 ﻿"use strict";
 
-var TOUCH_DEBUG = false;
-
 // TouchPointManager(targetElement)
 // Initialize with the element that is the target of the events.
 //
@@ -44,9 +42,6 @@ Object.defineProperties(TouchPointManager.prototype, {
                 this._pointCount++;
                 this._points[pID] = new TouchPoint(e.clientX, e.clientY);
                 this._points[pID].down(e.clientX, e.clientY);
-                if(TOUCH_DEBUG) {
-                    console.log("Created touch point for pID: " + pID + ", at: " + this._points[pID].getCurrentPosition().x + ", " + this._points[pID].getCurrentPosition().y)
-                }
             }
         }
     },
@@ -55,10 +50,6 @@ Object.defineProperties(TouchPointManager.prototype, {
         value: function removeTouchPoint(e) {
             var pID = e.pointerId || 0;
             if(this._points[pID]) {
-                if(TOUCH_DEBUG) {
-                    console.log("Removing touch point for pID: " + pID + ", at: " + this._points[pID].getCurrentPosition().x + ", " + this._points[pID].getCurrentPosition().y)
-                }
-
                 this._points[pID].up(e.clientX, e.clientY);
 
                 delete this._points[pID];
@@ -76,10 +67,6 @@ Object.defineProperties(TouchPointManager.prototype, {
             var pID = e.pointerId || 0;
             if(this._points[pID]) {
                 this._points[pID].move(e.clientX, e.clientY);
-            }
-
-            if(TOUCH_DEBUG) {
-                console.log("Moving touch point for pID: " + pID + ", to: " + this._points[pID].getCurrentPosition().x + ", " + this._points[pID].getCurrentPosition().y)
             }
         }
     },
