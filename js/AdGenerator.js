dfc56405791229bf18d1787cda1c587f713e5ad9
Justin Rogers <justrog@microsoft.com>
2013-04-28 13:24:24 -0700

    Basic credits screen.
    Hook up ads to show the credits.

--- js/AdGenerator.js
+++ js/AdGenerator.js
@@ -3,9 +3,11 @@
 
 
 function AdGenerator() {
-    this._ads = ["Feeling out of Shape?  Try our new Circle Peg!",
-    "Are you a bit Round?  Try our new Square program! Guaranteed to tone your shape in 30 days or your money back!",
-    "Are you Blue?  We have a new Yellow for you!"];
+    this._ads = [
+        "Feeling out of Shape?  Try our new Circle Peg!",
+        "Are you a bit Round?  Try our new Square program! Guaranteed to tone your shape in 30 days or your money back!",
+        "Are you Blue?  We have a new Yellow for you!"
+    ];
     this._adSwitch = 1875;
     this.ct = 0;
     this.currentAd = Math.floor(Math.random()*this._ads.length);
@@ -17,32 +19,39 @@ function AdGenerator() {
 AdGenerator.prototype = Object.create(null);
 AdGenerator.prototype.constructor = AdGenerator;
 
-(Object.defineProperties(AdGenerator.prototype, {
-    processTick: {
-        value: function processTick() {
-            this.ct++;
-            if (this.ct > this._adSwitch) {
-                this.ct = 0;
-                var oldNumber = this.currentAd;
-                while (oldNumber == this.currentAd) {
-                    this.currentAd = Math.floor(Math.random() * this._ads.length);
-                }
-                this.displayAd(this._ads[this.currentAd]);
-            }
-        }
-    },
+(function () {
+    function _showCredits() {
+        PopupManager.showPopup("popupCredits");
+    }
 
-    displayAd: {
-        value: function displayAd(adText) {
-            var element = document.createElement("a");
-            var text = document.createTextNode(adText);
-            element.appendChild(text);
-            element.setAttribute('href', 'http://www.bing.com');
-            var div = document.getElementById('panelAd');
-            while (div.children.length > 0) {
-                div.removeChild(div.children[0]);
+    (Object.defineProperties(AdGenerator.prototype, {
+        processTick: {
+            value: function processTick() {
+                this.ct++;
+                if (this.ct > this._adSwitch) {
+                    this.ct = 0;
+                    var oldNumber = this.currentAd;
+                    while (oldNumber == this.currentAd) {
+                        this.currentAd = Math.floor(Math.random() * this._ads.length);
+                    }
+                    this.displayAd(this._ads[this.currentAd]);
+                }
             }
-            div.appendChild(element);
-        }
-    },
-}));
+        },
+
+        displayAd: {
+            value: function displayAd(adText) {
+                var element = document.createElement("a");
+                var text = document.createTextNode(adText);
+                element.style.cursor = "pointer";
+                element.appendChild(text);
+                element.addEventListener("click", _showCredits);
+                var div = document.getElementById('panelAd');
+                while (div.children.length > 0) {
+                    div.removeChild(div.children[0]);
+                }
+                div.appendChild(element);
+            }
+        },
+    }));
+})();
