2bb181978b39a7a8408909dabcf5b1b01d0d57a2
Matt Rakow <matt.rakow@gmail.com>
2013-04-27 18:01:32 -0700

    Adding TouchPoint class for storing gesture data

---
+++ js/TouchPoint.js
@@ -0,0 +1,41 @@
+﻿"use strict";
+
+// TouchPoint(x, y)
+// A single point of contact, stores data over time as needed to recognize gestures.  E.g. in the future might store a timestamp for down, if we need to recognize a hold.
+//
+function TouchPoint(x, y) {
+    this._x = x;
+    this._y = y;
+}
+
+TouchPoint.prototype = Object.create(null);
+TouchPoint.prototype.constructor = TouchPoint;
+
+Object.defineProperties(TouchPoint.prototype, {
+    down: {
+        value: function move(x, y) {
+            this._x = x;
+            this._y = y;
+        }
+    },
+
+    move: {
+        value: function move(x, y) {
+            this._x = x;
+            this._y = y;
+        }
+    },
+
+    up: {
+        value: function up(x, y) {
+            this._x = x;
+            this._y = y;
+        }
+    },
+
+    getCoordinates: {
+        value: function getCoordinates() {
+            return {x : this._x, y : this._y};
+        }
+    }
+});
