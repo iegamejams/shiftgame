def3b8879bd4d6f04bb18501d1bf9e8493c22bc4
Matt Rakow <matt.rakow@gmail.com>
2013-04-27 17:29:11 -0700

    TouchPointManager matching conventions

--- js/TouchPointManager.js
+++ js/TouchPointManager.js
@@ -4,65 +4,79 @@
 // Initialize with the element that is the target of the events.
 //
 function TouchPointManager(targetElement) {
-    this.points = [];
-    this.pointCount = 0;
+    this._targetElement = targetElement;
+    this._points = [];
+    this._pointCount = 0;
 
     // Determine correct events to register for
     if(navigator.msPointerEnabled) {
-        downevent = "MSPointerDown";
-        upevent = "MSPointerUp";
-        moveevent = "MSPointerMove";
+        this._downevent = "MSPointerDown";
+        this._upevent = "MSPointerUp";
+        this._moveevent = "MSPointerMove";
         document.addEventListener("MSPointerCancel", this.removeTouchPoint.bind(this), false);
         document.addEventListener("MSHoldVisual", function(e) { e.preventDefault(); }, false);
     }
     else {
-        downevent = "mousedown";
-        upevent = "mouseup";
-        moveevent = "mousemove";
+        this._downevent = "mousedown";
+        this._upevent = "mouseup";
+        this._moveevent = "mousemove";
     }
-    
+
     // Register invariant events
-    targetElement.addEventListener(downevent, this.addTouchPoint.bind(this), false);
-    document.addEventListener(upevent, this.removeTouchPoint.bind(this), false);
+    this._targetElement.addEventListener(this._downevent, this.addTouchPoint.bind(this), false);
+    document.addEventListener(this._upevent, this.removeTouchPoint.bind(this), false);
 }
 
-TouchPointManager.prototype.addTouchPoint = function(e) {
-    if(this.pointCount == 0) {
-        document.addEventListener(moveevent, this.moveTouchPoint.bind(this), false);
-    }
+TouchPointManager.prototype = Object.create(null);
+TouchPointManager.prototype.constructor = TouchPointManager;
 
-    var pID = e.pointerId || 0;
-    if(!this.points[pID]) {
-        this.pointCount++;
-        this.points[pID] = {x : e.clientX, y : e.clientY};
-    }
-}
+Object.defineProperties(TouchPointManager.prototype, {
+    addTouchPoint: {
+        value: function addTouchPoint(e) {
+            if(this._pointCount == 0) {
+                document.addEventListener(this._moveevent, this.moveTouchPoint.bind(this), false);
+            }
 
-TouchPointManager.prototype.removeTouchPoint = function(e) {
-    var pID = e.pointerId || 0;
-    if(this.points[pID]) {
-        delete this.points[pID];
-        this.pointCount--;
-        
-        if(this.pointCount == 0) {
-            document.removeEventListener(moveevent, this.moveTouchPoint, false);
+            var pID = e._pointerId || 0;
+            if(!this._points[pID]) {
+                this._pointCount++;
+                this._points[pID] = {x : e.clientX, y : e.clientY};
+            }
+        }
+    },
+
+    removeTouchPoint: {
+        value: function removeTouchPoint(e) {
+            var pID = e._pointerId || 0;
+            if(this._points[pID]) {
+                delete this._points[pID];
+                this._pointCount--;
+                
+                if(this._pointCount == 0) {
+                    document.removeEventListener(this._moveevent, this.moveTouchPoint, false);
+                }
+            }
+        }
+    },
+
+    moveTouchPoint: {
+        value: function moveTouchPoint(e) {
+            var pID = e._pointerId || 0;
+            if(this._points[pID]) {
+                this._points[pID].x = e.clientX;
+                this._points[pID].y = e.clientY;
+            }
+        }
+    },
+
+    // TouchPointManager.doAsEachPoint(funcHandle, args)
+    // Pass funcHandle that should be called in the context of each point, and any args required.
+    //
+    doAsEachPoint: {
+        value: function doAsEachPoint(funcHandle, args) {
+            this._points.forEach(function(point) {
+                funcHandle.bind(point, args)();
+            });
         }
     }
-}
-
-TouchPointManager.prototype.moveTouchPoint = function(e) {
-    var pID = e.pointerId || 0;
-    if(this.points[pID]) {
-        this.points[pID].x = e.clientX;
-        this.points[pID].y = e.clientY;
-    }
-}
-
-// TouchPointManager.doAsEachPoint(funcHandle, args)
-// Pass funcHandle that should be called in the context of each point, and any args required.
-//
-TouchPointManager.prototype.doAsEachPoint = function(funcHandle, args) {
-    this.points.forEach(function(point) {
-        funcHandle.bind(point, args)();
-    });
-}
+});
